ceb00eccd6a6d13993986802889e078f17abcbaf
Roger <rogervz.1710@gmail.com>
2026-05-27 11:39:11 -0500

    init

---
+++ src/test/java/com/weg/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weg.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
